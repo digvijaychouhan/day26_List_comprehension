1f8881ac47a33e1ac204983c4d52d18fca721819
Digvijay Chouhan <digvijay@outlook.in>
2022-08-21 21:03:09 +0530

    Day26 - Nato program error handling commit

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 df = pandas.DataFrame(data)
 phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+# print(phonetic_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word for phonetic code: ").upper()
-output_list = [phonetic_dict[letter] for letter in user_input]
-print(output_list)
+while True:
+    user_input = input("Enter a word for phonetic code: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in user_input]
+        print(output_list)
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
